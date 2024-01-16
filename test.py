97d211b6b35698a73ad94bcd0ea7eb31485a3dc8
Malyshev Georgiy <georgiymalyshev@yandex.ru>
2024-01-16 18:03:17 +0400

    дино анимка + меню нади часть 2

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import pygame
-import os
 import random
+import os
 
 # constants
 running = False
@@ -9,7 +9,7 @@ SW = 1280
 SH = 633
 LEFT = 1
 RIGHT = 0
-ground = 500
+ground = 420
 fps = 60
 bg_speed = 5
 score = 0
@@ -17,7 +17,8 @@ ten_seconds = 0
 # color constants
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
+frame = 0
+on_ground = True
 
 screen = pygame.display.set_mode((SW, SH))
 background = (0, 150, 150)
@@ -34,27 +35,28 @@ for i in range(0, 1920, 960):
     desert_rect.left = i
     bg_list.append(desert_rect)
 
-ground_rect = (0, ground, SW, SH - ground)
+cac_onscreen_list = []
+cac1 = pygame.image.load('dino sprites/cactus1.png')
+cac1_rect = cac1.get_rect()
+cac2 = pygame.image.load('dino sprites/cactus2.png')
+cac2_rect = cac2.get_rect()
+cac3 = pygame.image.load('dino sprites/cactus3.png')
+cac3_rect = cac3.get_rect()
 
-logo = pygame.image.load('dino_log.jpg')
+logo = pygame.image.load('dinosaur game ultra deluxe logo.png')
 logo = pygame.transform.scale(logo, (1280, 633))
 logo_rect = logo.get_rect()
 logo_rect.center = (SW / 2, SH/2)
 
-dino_walk_list = [[], []]  # 1 направление, 2 номер кадра
-for i in range(1, 11):
-    dino = pygame.image.load('resources/walk' + str(i) + '.png')
-    dino = pygame.transform.scale(dino, (100, 100))
-    dino_walk_list[RIGHT].append(dino)
-for i in range(1, 11):
-    dino = pygame.image.load('resources/walk' + str(i) + '.png')
-    dino = pygame.transform.scale(dino, (100, 100))
-    dino = pygame.transform.flip(dino, True, False)
-    dino_walk_list[LEFT].append(dino)
+dino_walk_list = []  # 1 направление, 2 номер кадра
+for i in range(1, 3):
+    dino = pygame.image.load('dino sprites/dino walk' + str(i) + '.png')
+    dino = pygame.transform.scale(dino, (80, 80))
+    dino_walk_list.append(dino)
 dino_rect = dino.get_rect()
 dino_rect.x = 0
 dino_rect.bottom = ground + 1
-dino_rect.left = 200
+dino_rect.left = 100
 
 font = pygame.font.SysFont('arial', 30)
 pixel_font = pygame.font.SysFont('OCR A Extended', 30)
@@ -86,7 +88,6 @@ while start:
             os.environ['SDL_VIDEO_CENTERED'] = '0'
             screen1 = pygame.display.set_mode((SW, SH))
 
-
         if event.type == pygame.MOUSEBUTTONDOWN and quit_text_rect.collidepoint(event.pos):
             start = False
 
@@ -100,8 +101,27 @@ while start:
 while running:
     timer.tick(fps)
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+            on_ground = False
+
+    for i in range(len(bg_list)):
+        bg_list[i].x -= bg_speed
+        if bg_list[i].right < 0:
+            bg_list[i].left = SW
+        screen1.blit(desert, bg_list[i])
+
     ten_seconds += 1
     if ten_seconds == 10:
+        if frame < len(dino_walk_list) - 1 and on_ground:
+            frame += 1
+        else:
+            frame = 0
+
         score += 1
         ten_seconds = 0
 
@@ -114,6 +134,8 @@ while running:
         if bg_list[i].right < 0:
             bg_list[i].left = SW
         screen1.blit(desert, bg_list[i])
+
+    screen1.blit(dino_walk_list[frame], dino_rect)
 
     score_text = pixel_font.render(f'{score}', True, BLACK)
     screen1.blit(score_text, (720, 12))
